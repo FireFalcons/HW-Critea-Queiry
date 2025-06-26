5817baf20c8b6141d23fe2a7c3ea4a7491226c38
Andriy <kozakua51@gmail.com>
2025-06-26 13:52:36 +0300

    Initial commit

---
+++ Create-search-book/src/main/java/com/example/createsearchbook/CreateSearchBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.createsearchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateSearchBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateSearchBookApplication.class, args);
+    }
+
+}
